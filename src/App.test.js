2a802945c2b7a4539229171aebd469b50b0bcad7
Thanisors <6206021621031@fitm.kmutnb.ac.th>
2022-03-27 02:15:40 +0700

    push

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Home from './Pages/Home/home.component';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('poom', () => {
+  render(<Home />);
+  const linkElement = screen.getByText(/poom/i);
   expect(linkElement).toBeInTheDocument();
 });
